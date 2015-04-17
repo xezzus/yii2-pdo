745d24873caaa28f2eb12b7cc28bbd094c681daf
Buturlakin Andrey <xezzus@gmail.com>
2015-04-17 09:28:43 +0300

    first commit

---
+++ Connection.php
@@ -0,0 +1,13 @@
+<?php
+namespace yii\pdo;
+
+use yii\base\Component;
+
+class Connection extends Component {
+
+    public function __construct($config=[]){
+        var_dump($config);
+    }
+
+}
+?>
