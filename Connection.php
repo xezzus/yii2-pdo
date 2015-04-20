11f89b425b528258e84a21f44e26733f3ebba469
xezzus <xezzus@gmail.com>
2015-04-20 13:25:18 +0300

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -7,20 +7,37 @@ class Connection{
 
     private $connection;
 
-    public function srv($name=null){
+    public function createConnect(){
 
         if(!isset($this->servers[$name])) die('This server pdo is not configure');
 
         if(!$this->connection){
             $this->connection = new PDO(
-                $this->servers[$name]['dsn'],
-                $this->servers[$name]['user'],
-                $this->servers[$name]['pass']
+                $this->['dsn'],
+                $this->['username'],
+                $this->['password']
             );
             $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         }
 
         return $this->connection;
+    }
+    
+    public functino fetch($sql){
+        $cn = $this->createConnection();
+        $q = $cn->query($sql);
+        if($q) return $q->fetch();
+    }
+    
+    public functino fetchAll($sql){
+        $cn = $this->createConnection();
+        $q = $cn->query($sql);
+        if($q) return $q->fetchAll();
+    }
+
+    public functino exec($sql){
+        $cn = $this->createConnection();
+        return $cn->exec($sql);
     }
 
 }
